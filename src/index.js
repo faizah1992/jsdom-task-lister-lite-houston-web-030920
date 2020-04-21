628c84e7ce74545335e62145692f78717b56b0bf
faizah1992 <faizah1992@gmail.com>
2020-04-21 16:33:53 -0500

    strech

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const handleSubmit = function(e){
+   e.preventDefault()
+   let taskDescription = document.querySelector("#new-task-description")
+   let description = (taskDescription.value)
+
+   let taskPriority = document.querySelector("quantity")
+   let priority = (taskPriority.value)
+  
+   let item = document.createElement("option")
+   item.innerText = description
+   let p = document.createElement()
+   debugger
+   document.getElementById("option").append(item)
+ }
+
+let form = document.querySelector("#create-task-form")
+form.addEventListener("submit", handleSubmit)
+
+function deletetasks() {
+  let t = document.getElementById("option");
+  t.remove(t.selectedIndex);
+}
+
+
+
